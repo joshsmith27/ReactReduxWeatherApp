861266ae589101e1acc21b71bf336fd22d9840a3
Joshua Richard Smith <veryjoshua@gmail.com>
2017-03-18 10:04:54 -0600

    heroku hosting

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY ='d426be2492e59810f1145b9fd797d0fe';
-const ROOT_URL =`http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL =`https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
